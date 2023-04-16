b2cffda972c947cd9641ca9d12cefb0c20c0d03f
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-16 14:49:21 -0600

    Route: update routes and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+    @chef_1 = Chef.create!(name: "Chef Boyardee")
+
+    @dish_1 = @chef_1.dishes.create!(name: "Ravioli", description: "Canned pasta", chef_id: @chef_1.id)
+    @dish_2 = @chef_1.dishes.create!(name: "Spaghetti", description: "Pasta with red sauce", chef_id: @chef_1.id)
+
+    @ingredient_1 = Ingredient.create!(name: "Pasta", calories: 100)
+    @ingredient_2 = Ingredient.create!(name: "Tomato Sauce", calories: 50)
+    @ingredient_3 = Ingredient.create!(name: "Cheese", calories: 200)
+    @ingredient_4 = Ingredient.create!(name: "Meat", calories: 300)
+
+    @dish_ingredient_1 = DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @ingredient_1.id)
+    @dish_ingredient_1 = DishIngredient.create!(dish_id: @dish_1.id, ingredient_id: @ingredient_2.id)
+    @dish_ingredient_1 = DishIngredient.create!(dish_id: @dish_2.id, ingredient_id: @ingredient_3.id)
+    @dish_ingredient_1 = DishIngredient.create!(dish_id: @dish_2.id, ingredient_id: @ingredient_4.id) 
