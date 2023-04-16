c5739c74be9a6fdc984b6ef5d4e55550fd5f83d7
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-04-16 15:03:21 -0600

    Test: update test data and write test for chef name

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe Dish, type: :model do
   before(:each) do
     @chef_1 = Chef.create!(name: "Chef Boyardee")
+    @chef_2 = Chef.create!(name: "Chef Ramsey")
 
     @dish_1 = @chef_1.dishes.create!(name: "Ravioli", description: "Canned pasta", chef_id: @chef_1.id)
-    @dish_2 = @chef_1.dishes.create!(name: "Spaghetti", description: "Pasta with red sauce", chef_id: @chef_1.id)
+    @dish_2 = @chef_1.dishes.create!(name: "Spaghetti", description: "Pasta with red sauce", chef_id: @chef_2.id)
 
     @ingredient_1 = Ingredient.create!(name: "Pasta", calories: 100)
     @ingredient_2 = Ingredient.create!(name: "Tomato Sauce", calories: 50)
@@ -34,6 +35,13 @@ RSpec.describe Dish, type: :model do
       it 'returns the total calories for a dish' do
         expect(@dish_1.total_calories).to eq(150)
         expect(@dish_2.total_calories).to eq(500)
+      end
+    end
+
+    describe "#chef_name" do
+      it 'returns the name of the chef for a dish' do
+        expect(@dish_1.chef_name).to eq("Chef Boyardee")
+        expect(@dish_2.chef_name).to eq("Chef Ramsey")
       end
     end
   end
